d6c80a7c1952a65dc501080cba2c29160dc96fb4
hamzus2611 <hamza.gharsallah@isimg.tn>
2021-05-07 22:30:13 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { Form, Button } from 'react-bootstrap'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 style={{color:"red",fontSize:"30px"}}>Se connecter</h1>
+      <Form className="col-md-5">
+        <Form.Group controlId="formBasicEmail">
+          
+          <Form.Control type="email" placeholder="Adresse E-mail" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+    </Form.Text>
+        </Form.Group>
+
+        <Form.Group controlId="formBasicPassword">
+         
+          <Form.Control type="password" placeholder="Mot de passe" />
+        </Form.Group>
+        <Form.Group controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          connexion
+  </Button>
+      </Form>
     </div>
   );
 }
